7b62748d5ad1b7cbc26130b89f8dad3b8bb5885c
gulablohani9-ui <gulablohani9@gmail.com>
2026-09-23 10:42:18 +0530

    Implement generateChart method in SBCLogic
    Added a method to generate a Sarvatobhadra Chakra chart.

---
+++ app/src/main/java/com/example/sbc/SBCLogic.java
@@ -0,0 +1,8 @@
+package com.example.sbc;
+
+public class SBCLogic {
+    public String generateChart(String date, String city) {
+        // TODO: Integrate Swiss Ephemeris calculations here
+        return "Sarvatobhadra Chakra chart for " + date + " at " + city;
+    }
+}
